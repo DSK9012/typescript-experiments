d2b610deff259b2e3e7cb440b17bfbde9e64e5b6
DSK9012 <saikiran.dannana@gmail.com>
2022-09-02 09:04:09 +0530

    updated comment for union types vs generics

--- generics/generic-classes.ts
+++ generics/generic-classes.ts
@@ -9,7 +9,7 @@ class DataStorage<T extends string | number | boolean>{   // Its always better t
     return [...this.data];
   }
 
-  removeItem(item){
+  removeItem(item:T){
     const index = this.data.indexOf(item);
     this.data.splice(this.data.indexOf(item),1);
   }
@@ -28,3 +28,6 @@ string.removeItem('string2')
 string.getItems() // ['string1']
 
 // You see the benefit of using generics here. The same class can be used for multiple data types.Awesome na..!
+
+// Comparing to union types, Generic types help us to lock the incoming data at a root level like in above class.
+// Assume if you are accepting types using union like string[] | number[], then we will face difficulties in accessing methods. Note it. 
